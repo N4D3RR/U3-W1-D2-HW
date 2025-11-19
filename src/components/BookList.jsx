83f2a15718f9abc4c1322b10047040fca9695109
NAD3R <naderdeghaili@gmail.com>
2025-11-19 16:58:01 +0100

    completed 3

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,41 @@
 import { Container, Row, Col } from "react-bootstrap"
 import { Component } from "react"
 import SingleBook from "./SingleBook"
+import Form from "react-bootstrap/Form"
 
 class BookList extends Component {
-  state = {}
+  state = {
+    search: "",
+  }
   render() {
     const books = this.props.books
-    console.log("BOOKS PROP:", this.props.books)
+    console.log("LIBRI:", this.props.books)
+
+    const filteredBooks = books.filter((book) => {
+      return book.title.toLowerCase().includes(this.state.search)
+    })
+    console.log("LIBRI CERCATI:", filteredBooks)
     return (
       <Container className="mt-4">
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Cerca un libro"
+            value={this.state.search}
+            onChange={(e) => {
+              this.setState({ search: e.target.value })
+            }}
+          />
+        </Form.Group>
         <Row className="g-4">
-          {books.map((libri) => {
+          {/* {books.map((libri) => {
+            return (
+              <Col xs={6} sm={4} md={3} lg={2} key={libri.asin}>
+                <SingleBook book={libri} />
+              </Col>
+            )
+          })} */}
+          {filteredBooks.map((libri) => {
             return (
               <Col xs={6} sm={4} md={3} lg={2} key={libri.asin}>
                 <SingleBook book={libri} />
